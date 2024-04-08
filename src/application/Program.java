730897892938b27c47d81b26ccb227c6e66de4f1
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-08 13:52:59 -0300

    Inserção de dados com loop e a forma correta de inserir datas no Banco de Dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,37 +3,63 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.sql.Date;
 
 import db.DB;
-import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		Connection conn = null;
 		PreparedStatement st = null;
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		try {
 			conn = DB.getConnection();
 
-			st = conn.prepareStatement(
-					"DELETE FROM seller " 
-					+ "WHERE " 
-					+ "Name= ? ");
+			st = conn.prepareStatement("INSERT INTO seller " 
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " 
+					+ "(?, ?, ?, ?, ?)");
 
-			//st.setInt(1, 9);
-			st.setString(1, "Lindomar Rose");
+			System.out.print("Quantos vendedores quer adicionar? ");
+			int op = sc.nextInt();
+			
+			for (int i = 0; i < op; i++) {
+				sc.nextLine();
+				System.out.print("Digite o nome: ");
+				st.setString(1, sc.nextLine());
+				System.out.print("Digite o email: ");
+				st.setString(2, sc.nextLine());
+				System.out.print("Digite o aniversário: ");
 
+				st.setDate(3, new Date(sdf.parse(sc.nextLine()).getTime()));
+				
+				System.out.print("Digite o salário base: ");
+				st.setDouble(4, sc.nextDouble());
+				System.out.print("Digite o ID do Departamento: ");
+				st.setDouble(5, sc.nextInt());
+			}
+			
 			int rowsAffected = st.executeUpdate();
 
 			System.out.println("Done! Rows affected: " + rowsAffected);
 
 		} catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} finally {
+			e.printStackTrace();
+		}catch(ParseException e) {
+			e.getMessage();
+		}
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
+			sc.close();
 		}
 
 	}
