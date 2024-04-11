a37d3da95868b1084c34cb37fa972b0f97e8570f
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-11 15:21:07 -0300

    Deleção de itens na tabela com mais de uma condição para limitar a deleção

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,10 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
-import java.sql.Date;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -18,43 +16,24 @@ public class Program {
 		Connection conn = null;
 		PreparedStatement st = null;
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		try {
 			conn = DB.getConnection();
 
-			st = conn.prepareStatement("INSERT INTO seller " 
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES " 
-					+ "(?, ?, ?, ?, ?)");
-
-			System.out.print("Quantos vendedores quer adicionar? ");
-			int op = sc.nextInt();
+			st = conn.prepareStatement("DELETE FROM seller "
+					+ "WHERE "
+					+ "Name = ? "
+					+ "AND "
+					+ "Id = ?");
 			
-			for (int i = 0; i < op; i++) {
-				sc.nextLine();
-				System.out.print("Digite o nome: ");
-				st.setString(1, sc.nextLine());
-				System.out.print("Digite o email: ");
-				st.setString(2, sc.nextLine());
-				System.out.print("Digite o aniversário: ");
+			st.setString(1, "Carl Purple");
+			st.setInt(2, 13);
 
-				st.setDate(3, new Date(sdf.parse(sc.nextLine()).getTime()));
-				
-				System.out.print("Digite o salário base: ");
-				st.setDouble(4, sc.nextDouble());
-				System.out.print("Digite o ID do Departamento: ");
-				st.setDouble(5, sc.nextInt());
-			}
-			
 			int rowsAffected = st.executeUpdate();
 
 			System.out.println("Done! Rows affected: " + rowsAffected);
 
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}catch(ParseException e) {
-			e.getMessage();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
